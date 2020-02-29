f508a00a937b9ee696ed7204af98ad02f26ebc97
Dubiousjim <dubiousjim@gmail.com>
2020-02-29 11:42:08 -0500

    Require trunc len to be u63
    The len parameter to truncate was u64. Odd to use a bigger type for
    truncate than for seek (where we were effectively letting offset be
    u32); and indeed the underlying libc expects an off_t, which is i64.
    
    Instead made this variable i64, requiring it to be non-negative (so
    effectively u63). This matches the treatment of offset for op_seek when
    whence is SEEK_START.

--- cli/ops/fs.rs
+++ cli/ops/fs.rs
@@ -760,7 +760,7 @@ fn op_read_link(
 struct TruncateArgs {
   promise_id: Option<u64>,
   path: String,
-  len: u64,
+  len: i64,
   mode: Option<u32>,
   create: bool,
   create_new: bool,
@@ -773,7 +773,8 @@ fn op_truncate(
 ) -> Result<JsonOp, OpError> {
   let args: TruncateArgs = serde_json::from_value(args)?;
   let path = deno_fs::resolve_from_cwd(Path::new(&args.path))?;
-  let len = args.len;
+  // require len to be 63 bit unsigned
+  let len: u64 = args.len.try_into()?;
   let create = args.create;
   let create_new = args.create_new;
 
