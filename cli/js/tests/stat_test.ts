f29c5cc4eea0e9495c873ee81260db567c1777b5
Dubiousjim <dubiousjim@gmail.com>
2020-02-29 11:43:13 -0500

    Add FileInfo.anyModified
    This reports the `ctime` field from Unix (and that's what the field is
    named in the internal StatResponse structure).
    
    Better names to use on the TypeScript side are welcome. `ctime` is
    problematic because it often gets confused with creation-time.
    `metadataModified` is a reasonable candidate, because this field (unlike
    `modified`) gets refreshed whenever a file is chmod-ed, etc. But this
    field is *also* refreshed whenever data is written. (So it will always
    be at least as new as `modified`.)

--- cli/js/tests/stat_test.ts
+++ cli/js/tests/stat_test.ts
@@ -189,6 +189,7 @@ unitTest(
     assert(s.rdev === null);
     assert(s.blksize === null);
     assert(s.blocks === null);
+    assert(s.anyModified === null);
   }
 );
 
@@ -213,5 +214,6 @@ unitTest(
     assert(s.rdev !== null);
     assert(s.blksize !== null);
     assert(s.blocks !== null);
+    assert(s.anyModified !== null);
   }
 );
