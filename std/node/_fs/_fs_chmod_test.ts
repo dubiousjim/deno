9ea3e13e852176f3f481a0a09a04ffee1efd34f4
Dubiousjim <dubiousjim@gmail.com>
2020-03-15 20:21:06 -0400

    hacks

--- std/node/_fs/_fs_chmod_test.ts
+++ std/node/_fs/_fs_chmod_test.ts
@@ -18,7 +18,7 @@ test({
       .then(() => {
         const newFileMode: number | null = Deno.lstatSync(tempFile).mode;
         assert(newFileMode && originalFileMode);
-        assert((newFileMode & 0o7777) === 0o0777 && newFileMode > originalFileMode);
+        assert(newFileMode & 0o7777 == 0o0777 && newFileMode > originalFileMode);
       })
       .catch(() => {
         fail();
