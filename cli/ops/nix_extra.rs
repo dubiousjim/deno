9da8484b5672860757c3aae38e54ab25dd335c57
Dubiousjim <dubiousjim@gmail.com>
2020-03-29 08:39:37 -0400

    statx

--- cli/ops/nix_extra.rs
+++ cli/ops/nix_extra.rs
@@ -320,9 +320,9 @@ fn cstr(path: &Path) -> std::io::Result<CString> {
 }
 */
 
-fn result_nix_path<P: NixPath, F>(_p: P, _f: F) -> Result<T>
-    where F: FnOnce(&Cstr) -> T {
-  Err(Error::last())
+fn result_nix_path<P: NixPath, T, F>(_p: P, _f: F) -> Result<T>
+    where F: FnOnce(&CStr) -> T {
+  Err(nix::Error::last()) // FIXME
 }
 
 #[allow(dead_code)]
