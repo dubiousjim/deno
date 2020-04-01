3c989e73c663f76df56928b8da832b34c00f02d9
Dubiousjim <dubiousjim@gmail.com>
2020-04-01 09:34:22 -0400

    Upgrade rustyline 6.1.0, nix 0.17

--- cli/signal.rs
+++ cli/signal.rs
@@ -2,10 +2,13 @@
 use crate::op_error::OpError;
 
 #[cfg(unix)]
+use std::convert::TryFrom;
+
+#[cfg(unix)]
 pub fn kill(pid: i32, signo: i32) -> Result<(), OpError> {
   use nix::sys::signal::{kill as unix_kill, Signal};
   use nix::unistd::Pid;
-  let sig = Signal::from_c_int(signo)?;
+  let sig = Signal::try_from(signo)?;
   unix_kill(Pid::from_raw(pid), Option::Some(sig)).map_err(OpError::from)
 }
 
